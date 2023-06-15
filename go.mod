ecf04afe3e7ae12b2c7cdf7972615ce8d9814697
sudhirkFujitsu <sudhir.kadam@fujitsu.com>
2023-06-15 10:23:34 +0530

    final code with keys

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module server
 
-go 1.20
+go 1.18
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 )
--- server.go
+++ server.go
@@ -1,140 +1,221 @@
 package main
 
 import (
-	"crypto/rsa"
-	"crypto/x509"
 	"encoding/json"
-	"encoding/pem"
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
-
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gorilla/mux"
 )
 
-var (
-	// Define your self-made RS256 key
-	rsaPrivateKey = `
------BEGIN RSA PRIVATE KEY-----
-<YOUR RSA PRIVATE KEY>
------END RSA PRIVATE KEY-----
-`
+var users = map[string]string{
+	"sudhier": "password",
+	"sudhier1": "password1",
+}
 
-	// Create a global signing key instance
-	signingKey *rsa.PrivateKey
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
 
-	// Define the client credentials
-	clientID     = "your-client-id"
-	clientSecret = "your-client-secret"
-)
+var keyFiles = map[string]string{
+	"public":  "keys/jwtRSA256-public.pem",
+	"private": "keys/jwtRSA256-private.pem",
+}
 
-func init() {
-	// Parse the RSA private key
-	block, _ := pem.Decode([]byte(rsaPrivateKey))
-	if block == nil || block.Type != "RSA PRIVATE KEY" {
-		log.Fatal("Failed to decode PEM block containing RSA private key")
+type IntrospectiveResponse struct {
+	Active     bool    `json:"active"`
+	Username   string  `json:"username"`
+	Type       string  `json:"token_type"`
+	Expiration float64 `json:"exp"`
+}
+
+type TokenData struct {
+	TokenString string `json:"token"`
+}
+
+func Token(username string) (string, time.Time, error) {
+	return TokenWithExpiration(username, 5*time.Minute)
+}
+
+func TokenWithExpiration(username string, expiration time.Duration) (string, time.Time, error) {
+	prvKey, readErr := os.ReadFile(keyFiles["private"])
+	if readErr != nil {
+		return "", time.Now(), readErr
 	}
 
-	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	jwtKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(prvKey))
 	if err != nil {
-		log.Fatal("Failed to parse RSA private key: ", err)
+		return "", time.Now(), err
 	}
-	signingKey = key
+
+	expirationTime := time.Now().Add(expiration)
+
+	claims := &Claims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
+
+	return tokenString, expirationTime, err
+
+}
+
+func SignIn(w http.ResponseWriter, r *http.Request) {
+	username, password, ok := r.BasicAuth()
+	if ok {
+		expectedPassword, okUser := users[username]
+
+		if !okUser || expectedPassword != password {
+			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+	} else {
+		w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	tokenString, expirationTime, err := Token(username)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+
+	w.Write([]byte(fmt.Sprintf("Your current token is: %s", tokenString)))
+}
+
+func TokenCheck(r *http.Request) (int, *Claims) {
+	claims := &Claims{}
+
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			return http.StatusUnauthorized, claims
+		}
+		return http.StatusBadRequest, claims
+	}
+
+	tknStr := c.Value
+
+	pubKey, _ := os.ReadFile(keyFiles["public"])
+	jwtKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(pubKey))
+
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid || strings.HasPrefix(err.Error(), jwt.ErrTokenExpired.Error()) {
+			return http.StatusUnauthorized, claims
+		}
+		return http.StatusBadRequest, claims
+	}
+	if !tkn.Valid {
+		return http.StatusUnauthorized, claims
+	}
+
+	return http.StatusOK, claims
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	httpStatus, claims := TokenCheck(r)
+	if httpStatus != http.StatusOK {
+		w.WriteHeader(httpStatus)
+		if httpStatus == http.StatusUnauthorized {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		}
+		if httpStatus == http.StatusBadRequest {
+			http.Error(w, "BasRequest", http.StatusBadRequest)
+		}
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("Welcome %s!", claims.Username)))
+}
+
+func Introspection(w http.ResponseWriter, r *http.Request) {
+	httpStatus, _ := TokenCheck(r)
+	if httpStatus != http.StatusOK {
+		w.WriteHeader(httpStatus)
+		if httpStatus == http.StatusUnauthorized {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		}
+		if httpStatus == http.StatusBadRequest {
+			http.Error(w, "BasRequest", http.StatusBadRequest)
+		}
+		return
+	}
+
+	var token TokenData
+	err := json.NewDecoder(r.Body).Decode(&token)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	pubKey, _ := os.ReadFile(keyFiles["public"])
+	jwtKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(pubKey))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("Ready key failed")))
+		return
+	}
+
+	claims := jwt.MapClaims{}
+	parsedToken, err := jwt.ParseWithClaims(token.TokenString, claims, func(parsedToken *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("Token Inspection failed!")))
+		return
+	}
+
+	var introResp = IntrospectiveResponse{
+		Active:     parsedToken.Valid,
+		Username:   claims["username"].(string),
+		Type:       parsedToken.Header["typ"].(string),
+		Expiration: claims["exp"].(float64),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(introResp)
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Expires: time.Now(),
+	})
+	w.Write([]byte(fmt.Sprintf("You are now loged out!")))
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hello sudhier!")))
 }
 
 func main() {
-	r := mux.NewRouter()
+	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/welcome", Welcome)
+	http.HandleFunc("/token", SignIn)
+	http.HandleFunc("/inspect", Introspection)
+	http.HandleFunc("/logout", Logout)
 
-	// Define the /token endpoint for issuing access tokens
-	r.HandleFunc("/token", TokenHandler).Methods("POST")
-
-	// Define the /keys endpoint for listing the signing keys
-	// r.HandleFunc("/keys", KeysHandler).Methods("GET")
-
-	// Define the /introspect endpoint for token introspection
-	r.HandleFunc("/introspect", IntrospectHandler).Methods("POST")
-
-	log.Fatal(http.ListenAndServe(":8080", r))
-}
-
-// TokenHandler generates and returns a JWT access token
-func TokenHandler(w http.ResponseWriter, r *http.Request) {
-	// Validate basic authentication
-	user, pass, _ := r.BasicAuth()
-	if user != clientID || pass != clientSecret {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	// Create the claims for the JWT token
-	claims := jwt.StandardClaims{
-		ExpiresAt: time.Now().Add(time.Hour).Unix(),
-	}
-
-	// Generate the access token
-	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-	signedToken, err := token.SignedString(signingKey)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Return the access token
-	response := map[string]string{
-		"access_token": signedToken,
-		"token_type":   "Bearer",
-		"expires_in":   "3600",
-	}
-	jsonResponse, _ := json.Marshal(response)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonResponse)
-}
-
-// // KeysHandler lists the signing keys
-// func KeysHandler(w http.ResponseWriter, r *http.Request) {
-// 	// Get the public key from the signing key
-// 	publicKey := signingKey.PublicKey
-// 	rsaPublicKey := publicKey.(*rsa.PublicKey)
-
-// 	// Encode the public key to PEM format
-// 	publicKeyBytes, err := x509.MarshalPKIXPublicKey(rsaPublicKey)
-// 	if err != nil {
-// 		w.WriteHeader(http.StatusInternalServerError)
-// 		return
-// 	}
-// 	pemEncodedKey := pem.EncodeToMemory(&pem.Block{
-// 		Type:  "PUBLIC KEY",
-// 		Bytes: publicKeyBytes,
-// 	})
-
-// 	// Return the public key
-// 	w.Header().Set("Content-Type", "application/x-pem-file")
-// 	w.WriteHeader(http.StatusOK)
-// 	w.Write(pemEncodedKey)
-// }
-
-// IntrospectHandler checks the validity of a token
-func IntrospectHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the token from the request body
-	tokenString := r.FormValue("token")
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return signingKey.Public(), nil
-	})
-	if err != nil || !token.Valid {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	// Return token details
-	response := map[string]interface{}{
-		"active": true,
-	}
-	jsonResponse, _ := json.Marshal(response)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonResponse)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
